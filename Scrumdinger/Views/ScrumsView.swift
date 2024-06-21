6aea89afa54ca83dda8f4e51b4b68079b77917c8
lucaselvaggio <luca.selvaggio@democom.it>
2024-06-21 17:15:41 +0200

    restored working files

--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -17,12 +17,34 @@ struct ScrumsView: View {
     let saveAction: ()->Void
     
     var body: some View {
-        
-        if #available(iOS 16.0, *) {
-            ScrumsStackView(saveAction: saveAction, scrums: $scrums)
-        } else {
-            NavigationView {
-                ScrumsList(scrums: $scrums, saveAction: saveAction)
+        NavigationStack {
+            /* The List needs a way to identify individual items in the collection (id: ...)
+             DailyScrum is conform to the Identifiable protocol for a more robust way to communicate identity.
+             Use binding to access to array and single scrum. */
+            List($scrums){ $scrum in
+                /*  Each row pushes to an individual destination.
+                 The Text view acts as a placeholder until you create the detail screen in the next section. */
+                NavigationLink(destination: DetailView(scrum: $scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {
+                    isPresentingNewScrumView = true
+                }){
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
+            .sheet(isPresented: $isPresentingNewScrumView) {
+                NewScrumSheet(scrums: $scrums, isPresentingNewScrumView: $isPresentingNewScrumView)
+            }
+            
+            //  to monitor the operational state of the app, and save user data when the value changes to inactive
+            .onChange(of: scenePhase) { phase in
+                if phase == .inactive { saveAction() }
             }
         }
         
